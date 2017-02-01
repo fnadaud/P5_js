a949fbe2e417a9651f13f805311aebb0f7c04ddd
Florian Nadaud <nadauflo@gmail.com>
2017-02-01 18:27:14 +0100

    Random Walker

---
+++ Random Walker/sketch.js
@@ -0,0 +1,32 @@
+var x;
+var y;
+
+function setup() {
+	createCanvas(400, 400);
+	x = width/2;
+	y = height/2;
+	background(51);
+}
+
+function draw() {
+	stroke(255);
+	strokeWeight(2);
+	point(x, y);
+
+	var r = floor(random(4));
+
+	switch (r) {
+		case 0:
+			x++;
+			break;
+		case 1:
+			x--;
+			break;
+		case 2:
+			y++;
+			break;
+		case 3:
+			y--;
+			break;
+	}
+}
