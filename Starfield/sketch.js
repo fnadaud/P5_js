a1827ad71c1c2bacda33b29d01b1e19ffc0a2f78
Florian Nadaud <nadauflo@gmail.com>
2017-02-03 00:15:56 +0100

    effet hypervitesse

--- Starfield/sketch.js
+++ Starfield/sketch.js
@@ -6,10 +6,11 @@ function setup() {
 	for (var i = 0; i < nbStars; i++) {
 		stars[i] = new Star();
 	}
+	background(0);
 }
 
 function draw() {
-	background(0);
+	background(0,50);
 	translate(width/2, height/2);
 	for (var i = 0; i < nbStars; i++) {
 		stars[i].update();
