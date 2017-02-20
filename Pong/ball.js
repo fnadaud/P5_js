5afc83e1449d7999460b0ff629ff4ca03a409cd2
fnadaud <nadauflo@gmail.com>
2017-02-20 16:02:08 +0100

    Ajout des barres

--- Pong/ball.js
+++ Pong/ball.js
@@ -2,6 +2,8 @@ function Ball(x, y) {
 	this.pos = createVector(x, y);
 	this.vel = createVector(0, 0);
 	this.acc = createVector(0, 0);
+	this.width = 20;
+	this.length = 20;
 
 	this.glue = false;
 
@@ -18,11 +20,17 @@ function Ball(x, y) {
 		this.vel.add(this.acc);
 		this.pos.add(this.vel);
 		this.acc.mult(0);
-		if (this.pos.x > width || this.pos.x < 0) {
-			this.pos.sub(this.vel);
-			this.vel.x = -this.vel.x;
-			this.pos.add(this.vel);
-		} else if (this.pos.y > height || this.pos.y < 0) {
+		if (this.pos.x > width){
+			this.pos = createVector(width/2 - 20, height/2 - 20);
+			this.vel = createVector(5, 0);
+			scoreP1++;
+		}
+		else if(this.pos.x < 0) {
+			this.pos = createVector(width/2 - 20, height/2 - 20);
+			this.vel = createVector(-5, 0);
+			scoreP2++;
+		}
+		else if (this.pos.y + this.length > height || this.pos.y < 0) {
 			this.pos.sub(this.vel);
 			if (this.glue) {
 				this.vel.y = 0;
@@ -35,6 +43,19 @@ function Ball(x, y) {
 
 	this.show = function() {
 		fill(255);
-		ellipse(this.pos.x, this.pos.y, 20, 20);
+		rect(this.pos.x, this.pos.y, this.width, this.length, 100)
+		//ellipse(this.pos.x, this.pos.y, this.width, this.length);
 	}
+
+	/*this.collision = function(bar){
+		if (this.pos.x < bar.pos.x + bar.width &&
+   this.pos.x + this.width > bar.pos.x &&
+   this.pos.y < bar.pos.y + bar.length &&
+   this.length + this.pos.y > bar.pos.y) {
+    	return true;
+		}
+		else{
+			return false;
+		}
+	}*/
 }
--- Pong/sketch.js
+++ Pong/sketch.js
@@ -1,24 +1,77 @@
 var ball;
+var bar1;
+var bar2;
+var scoreP1 = 0;
+var scoreP2 = 0;
+var pause = false;
 
 function setup() {
 	createCanvas(windowWidth - 20, windowHeight - 20);
-	ballVelocity = createVector(10, 20);
 	ball = new Ball(width / 2, height / 2);
-	ball.setVelocity(10, 20);
+	ball.setVelocity(5, 0);
+
+	bar1 = new Bar(5, height/2 - 40);
+	bar2 = new Bar(width - 5 - 20, height/2 - 40);
+	textSize(20);
+	bar2.setVelocity(10);
 }
 
 function draw() {
 	background(51);
+	bar2.move(ball);
+	if(keyIsDown(90)){
+		bar1.up();
+	}
+	else if(keyIsDown(83)){
+		bar1.down();
+	}
+	bar1.update();
+	bar1.show();
+	bar2.update();
+	bar2.show();
+
 	ball.update();
+	bar1.collision(ball);
+	bar2.collision(ball);
+	/*if(ball.collision(bar1)){
+		ball.vel.x = - ball.vel.x;
+		ball.update();
+	}
+	if(ball.collision(bar2)){
+		ball.vel.x = - ball.vel.x;
+		ball.update();
+		bar2.setReactTime();
+	}*/
 	ball.show();
-	//ball.applyForce(1);
+
+	/*if(keyIsDown(UP_ARROW)){
+		bar2.up();
+	}
+	else if(keyIsDown(DOWN_ARROW)){
+		bar2.down();
+	}*/
+
+	text(scoreP1, width/2 - 20, 20);
+	text(scoreP2, width/2 + 20, 20);
 }
 
 function mousePressed() {
 	if (ball.glue) {
 		ball.glue = false;
-		ball.setVelocity(ball.vel.x, 20);
+		ball.setVelocity(ball.vel.x, 10);
 	} else {
 		ball.glue = true;
 	}
+}
+
+function keyPressed() {
+if (keyCode === 32) { //SPACE
+    if (pause) {
+      loop();
+      pause = false;
+    } else {
+      noLoop();
+      pause = true;
+    }
+  }
 }
